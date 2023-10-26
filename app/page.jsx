424abdc771901e77d85e61488ffdbda8a3f02858
Tiago Alexandre <tsalexandre97@hotmail.com>
2023-10-26 14:05:38 -0300

    feat(login): removing data validation function

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col bg-white px-10 py-20 max-w-[90vh] shadow-xl rounded-lg">
       <h1 className="text-4xl w-full font-bold text-[#424242]">Subscribe newsletter</h1>
-      <p className="mb-6 text-sm">Receive updates directly to your WhatsApp or email. Our posts go live every Monday from 9am.</p>
+      <p className="mb-6 text-sm">Do not want to miss any news, updates, notice or any offer on our products, then please subscribe to our mailing list.</p>
       <FormSubscription />
     </main>
   )
